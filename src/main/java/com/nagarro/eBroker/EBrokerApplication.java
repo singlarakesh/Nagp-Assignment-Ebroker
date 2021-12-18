e0f21ea21098d1fbebf1f8147f48cd1c86cb48fe
Parusha Singla <parusha.singla@nagarro.com>
2021-12-18 22:20:29 +0530

    first commit

---
+++ src/main/java/com/nagarro/eBroker/EBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.eBroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/eBroker/EBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.eBroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
